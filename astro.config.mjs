b2ae87d4cf4178e184fa8ee1c8f392586547b844
Tegan Rauh <onlysayinghi@gmail.com>
2024-04-26 03:18:43 -0400

    Path for default language

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ export default defineConfig({
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
+    routing: {
+      prefixDefaultLocale: true,
+    },
   },
 });
--- src/pages/index.md
+++
@@ -1,4 +0,0 @@
----
-title: Hello!
-layout: ../layouts/Home.astro
----
