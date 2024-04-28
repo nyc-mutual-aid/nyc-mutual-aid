112a70e290a64294c6265db9d20c472a874d30d1
Tegan Rauh <onlysayinghi@gmail.com>
2024-04-28 00:05:46 -0400

    Add deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://tegandbiscuits.github.io',
+  base: 'nyc-mutual-aid',
   integrations: [tailwind()]
 });
