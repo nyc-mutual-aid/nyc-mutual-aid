af9e0476a2fc3b8ef5e940488188d74662721ad8
Tegan Rauh <onlysayinghi@gmail.com>
2024-04-28 21:00:58 -0400

    Readd base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nyc-mutual-aid.github.io',
-  base: '',
+  base: 'nyc-mutual-aid',
   integrations: [tailwind()]
 });
